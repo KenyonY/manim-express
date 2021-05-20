b54153b2d65808f5c631c2448e3a83a14227f3ba
k.y <kunyuany@163.com>
2021-05-20 13:28:45 +0800

    Rebase dev

--- manim_express/utils.py
+++ manim_express/utils.py
@@ -1,36 +1,52 @@
 from manimlib import *
 
 
-def m_line(x, y, z=None, axes=None):
+def m_line(x,
+           y,
+           z=None,
+           axes=None,
+           color=None,
+           width=None,
+           optacity=None,
+           background=None):
     """point data to line"""
     L = len(x)
     x, y = np.array(x), np.array(y)
     if z is None:
         z = np.zeros(x.shape)
     if axes is None:
-        last_little_line = Line([x[L - 1], y[L - 1], z[0]], [x[0], y[0], z[0]])
-        line = VGroup(
-            *[
-                Line([x[i], y[i], 0], [x[i + 1], y[i + 1]])
-                for i in range(len(x) - 1)
-            ], last_little_line)
+        # last_little_line = Line([x[L - 1], y[L - 1], z[0]], [x[0], y[0], z[0]])
+        line = VGroup(*[
+            Line([x[i], y[i], 0], [x[i + 1], y[i + 1]]).set_stroke(
+                color=color,
+                width=width,
+                opacity=optacity,
+                background=background) for i in range(len(x) - 1)
+        ])
     elif type(axes) is Axes:
-        last_little_line = Line(axes.c2p(x[L - 1], y[L - 1]),
-                                axes.c2p(x[0], y[0]))
-        line = VGroup(
-            *[
-                Line(axes.c2p(x[i], y[i]), axes.c2p(x[i + 1], y[i + 1]))
-                for i in range(len(x) - 1)
-            ], last_little_line)
+        # last_little_line = Line(axes.c2p(x[L - 1], y[L - 1]),
+        #                         axes.c2p(x[0], y[0]))
+        line = VGroup(*[
+            Line(axes.c2p(x[i], y[i]), axes.c2p(x[i + 1], y[
+                i + 1])).set_stroke(color=color,
+                                    width=width,
+                                    opacity=optacity,
+                                    background=background)
+            for i in range(len(x) - 1)
+        ])
     else:  # ThreeDAxes
-        last_little_line = Line(axes.c2p(x[L - 1], y[L - 1], z[0]),
-                                axes.c2p(x[0], y[0], z[0]))
-        line = VGroup(
-            *[
-                Line(axes.c2p(x[i], y[i], z[i]),
-                     axes.c2p(x[i + 1], y[i + 1], z[i]))
-                for i in range(len(x) - 1)
-            ], last_little_line)
+        # last_little_line = Line(axes.c2p(x[L - 1], y[L - 1], z[0]),
+        #                         axes.c2p(x[0], y[0], z[0])).set_stroke(
+        #     color=color, width=width, opacity=optacity, background=background)
+
+        line = VGroup(*[
+            Line(axes.c2p(x[i], y[i], z[i]), axes.c2p(x[i + 1], y[
+                i + 1], z[i])).set_stroke(color=color,
+                                          width=width,
+                                          opacity=optacity,
+                                          background=background)
+            for i in range(len(x) - 1)
+        ])
     return line
 
 
