87875ecd1a1835c3490128b21a30e75ba2a73d5f
k.y <kunyuany@163.com>
2022-02-27 23:49:42 +0800

    Improve configuration

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python
+import setuptools
 
-from __future__ import print_function
-from setuptools import setup
-
-pkgname = "manim_express"
-pkgdir = pkgname
-
-setup(
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+setuptools.setup(
     pbr=True,
     package_data={
-        pkgdir: [
+        "manim_express": [
             '*.yaml', '*.yml',
         ],
     },
